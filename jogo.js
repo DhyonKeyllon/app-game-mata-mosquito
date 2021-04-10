cdbe2c4b0c5e714ff5fe5759af2f3630ac64ab11
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-04-09 21:36:37 -0300

    Geração randomica do mosquito
    Gerei a posicao randomica do mosquito, também os lados randomicos e o tamanho randomico. Deixando assim, a aplicacao mais dinamica!

--- jogo.js
+++ jogo.js
@@ -1,11 +1,64 @@
 
-var altura = 0
-var largura = 0
+var altura = 0;
+var largura = 0;
 
 function ajustaTamanhoPalcoJogo() {
-    altura = window.innerHeight
-    largura = window.innerWidth
-    console.log(altura, largura)
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(altura, largura); //debug
 }
 
-ajustaTamanhoPalcoJogo()
+ajustaTamanhoPalcoJogo();
+
+function posicaoRandomica() {
+    // gerar valores aleatorios para posicoes
+    var posicaoX = Math.floor(Math.random()  * largura) - 90; // subtrair para que a imagem nao estoure na tela (saia da res.)
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    // posicionar para 0, caso a posicao seja 0 e estoura com o valor -90
+    posicaoX = posicaoX < 0 ? 0 : posicaoX; // VALOR TERNARIO
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    console.log(posicaoX); //debug
+    console.log(posicaoY); //debug
+
+    // criar elemento html
+    var mosquito = document.createElement('img');
+    mosquito.src = 'imagens/mosquito.png';
+    // aplicar a classe de redimensionamento no elemento. Que foi definido no estilo.css
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+
+    // aplicar as posicao randomica pelas cordenadas
+    mosquito.style.left = posicaoX + 'px';
+    mosquito.style.top = posicaoY + 'px';
+    mosquito.style.position = 'absolute';
+    
+    // acessar o body para incluir a imagem dentro da pagina
+    document.body.appendChild(mosquito);
+}
+
+
+function tamanhoAleatorio() {
+
+    var classe = Math.floor(Math.random() * 3);
+
+    switch(classe) {
+        case 0:
+            return 'mosquito1';
+        case 1: 
+            return 'mosquito2';
+        case 2: 
+            return 'mosquito3';
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2);
+
+    switch(classe) {
+        case 0:
+            return 'ladoA';
+        case 1: 
+            return 'ladoB';
+    }
+}
